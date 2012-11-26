e41a1b06472302d6f71a2fb9eecbaad340e37576
Salvatore Rinchiera <srinchiera@college.harvard.edu>
2012-11-26 11:57:37 -0500

    Using Django Model Form

---
+++ aphfta/memdb/models.py
@@ -0,0 +1,106 @@
+from django.db import models
+from django.forms import ModelForm
+
+# Create your models here.
+class Facility(models.Model):
+    date = models.DateTimeField('Date Joined')
+    facility_name = models.CharField('Facility Name', max_length=200)
+    address = models.CharField('Address', max_length=250)
+    tel_office = models.IntegerField('Tel No. Office')
+    tel_mobile = models.IntegerField('Tell No. Mobile')
+    fax = models.IntegerField('Fax')
+    email = models.EmailField('Email')
+    moh_reg_cert = models.IntegerField('Ministry of Health Facility Registration Certificate No.')
+    FACILITY_TYPE = (
+           ("VO", 'Vountary Agency'),
+           ("PR", 'Private'),
+           ("CH", 'Charitable Organisation')
+    )
+    facility_type = models.CharField('Facility Type', max_length=2,
+                                     choices=FACILITY_TYPE)
+
+    doctor_ic = models.CharField('Doctor In Charge', max_length=250)
+    qualifications = models.CharField('Qualifications', max_length=500)
+    outpatient = models.CharField('Outpatient', max_length=500)
+    inpatient = models.CharField('Impatient', max_length=500)
+
+    hospital_health_maternity = models.BooleanField('Hospital, Health Centre, Dispensary, Maternity Homes')
+
+    major_operation_theaters = models.IntegerField('Major Operation Theatres')
+    minor_operation_theaters = models.IntegerField('Minor Operation Theatres')
+    delivery_beds = models.IntegerField('Delivery Beds')
+
+    antenatal_mch_serices = models.CharField('Antenatal / MCH Services', max_length=500)
+    other_speciality_services = models.CharField('Other Facility/Speciality Services', max_length=500)
+
+    lab = models.BooleanField('Laboratory')
+    xray = models.BooleanField('X-Ray Services')
+    blood_bank = models.BooleanField('Blood Bank')
+    pharmacy = models.BooleanField('Pharmacy')
+    dental = models.BooleanField('Dental Services')
+    ultrasonography = models.BooleanField('Ultrasonography')
+    physiotherapy = models.BooleanField('Physiotherapy')
+    icu = models.BooleanField('ICU')
+    ambulance = models.BooleanField('Ambulance Services')
+
+    physicians = models.IntegerField('Physicians')
+    surgeons = models.IntegerField('Surgeons')
+    obstetricians = models.IntegerField('Obstetricians')
+    paediatricians = models.IntegerField('Paediatricians')
+
+    medical_officers = models.IntegerField('Medical Officers')
+    assistant_medical_officers = models.IntegerField('Assistant Medical Officers')
+    clinical_officers = models.IntegerField('Assistant Medical Officers')
+    assistant_clinical_officers = models.IntegerField('Clinical Officers')
+    dental_officers = models.IntegerField('Dental Officers')
+    dental_assistants = models.IntegerField('Dental Assistants')
+
+    nurses_grade_A = models.IntegerField('Nurses Grade A')
+    nurses_grade_B = models.IntegerField('Nurses Grade B')
+    nursing_assistants = models.IntegerField('Nursing Assistants')
+    ward_attendants = models.IntegerField('Ward Attendants')
+
+    lab_technologists = models.IntegerField('Lab Technologists')
+    lab_technicians = models.IntegerField('Lab Technicians')
+    lab_assistants = models.IntegerField('Lab Assistants')
+
+    rad_technologists = models.IntegerField('Radiographic Technologists')
+    rad_technicians = models.IntegerField('Radiographic Technicians')
+    rad_assistants = models.IntegerField('Radiographic Assistants')
+
+    avg_outpatients_daily = models.IntegerField('Average Outpatients Seen /Day')
+    avg_inpatients_daily = models.IntegerField('Average Inpatients Seen /Day')
+    avg_deliveries_month = models.IntegerField('Average Deliveries /Month')
+    avg_mch_attendance_month = models.IntegerField('Average MCH Attendance /Month')
+
+    def __unicode__(self):
+        return self.facility_name
+
+class OtherStaff(models.Model):
+    staff_type = models.CharField(max_length = 250)
+    num = models.IntegerField()
+    facility = models.ForeignKey(Facility)
+    def __unicode__(self):
+        return self.staff_type
+
+class Ward(models.Model):
+    ward_type = models.CharField(max_length = 200)
+    num_beds = models.IntegerField()
+    facility = models.ForeignKey(Facility)
+    def __unicode__(self):
+        return self.ward_type
+
+class FacilityForm(ModelForm):
+    class Meta:
+        model = Facility
+        exclude = ('date', 'hospital_health_maternity',)
+
+class OtherStaffForm(ModelForm):
+    class Meta:
+        model = OtherStaff
+
+class WardForm(ModelForm):
+    class Meta:
+        model = Ward
+
+
---
+++ aphfta/memdb/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404, render_to_response
+from django.template import Context, loader
+from django.core.urlresolvers import reverse
+
+from memdb.models import Facility, OtherStaff, Ward
+from memdb.models import FacilityForm, OtherStaffForm, WardForm
+
+def index(request):
+    return render(request, 'memdb/index.html')
+
+def register(request):
+    facility_name_pos = 0
+    outpatient_pos = 10
+    lab_pos = 17
+    physicians_pos = 26
+    average_pos = 46
+
+    form = list(FacilityForm())
+
+    section1 = form[facility_name_pos:outpatient_pos]
+    section2 = form[outpatient_pos:lab_pos]
+    section3 = form[lab_pos:physicians_pos]
+    section4 = form[physicians_pos:average_pos]
+    section5 = form[average_pos:]
+
+    return render_to_response('memdb/register.html',
+            {'section1': section1,
+             'section2': section2,
+             'section3': section3,
+             'section4': section4,
+             'section5': section5
+            })
