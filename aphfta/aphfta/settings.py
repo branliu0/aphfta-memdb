2a4129a4994eab75819f9978237b5ca4bd222832
Brandon Liu <brandon.k.liu@gmail.com>
2012-12-25 16:13:22 +0300

    Add django.contrib.contenttypes back in

--- aphfta/aphfta/settings.py
+++ aphfta/aphfta/settings.py
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     'django.contrib.auth',
+    'django.contrib.contenttypes'
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
