3db8cafb051d637b83643155f707260508eff447
Salvatore Rinchiera <srinchiera@college.harvard.edu>
2012-12-25 02:32:11 -0500

    First day in Tanzania

--- aphfta/memdb/views.py
+++ aphfta/memdb/views.py
@@ -10,24 +10,28 @@ def index(request):
     return render(request, 'memdb/index.html')
 
 def register(request):
-    facility_name_pos = 0
-    outpatient_pos = 10
-    lab_pos = 17
-    physicians_pos = 26
-    average_pos = 46
 
-    form = list(FacilityForm())
+    if request.method == "GET":
+      facility_name_pos = 0
+      outpatient_pos = 10
+      lab_pos = 17
+      physicians_pos = 26
+      average_pos = 46
 
-    section1 = form[facility_name_pos:outpatient_pos]
-    section2 = form[outpatient_pos:lab_pos]
-    section3 = form[lab_pos:physicians_pos]
-    section4 = form[physicians_pos:average_pos]
-    section5 = form[average_pos:]
+      form = list(FacilityForm())
 
-    return render(request, 'memdb/register.html',
-            {'section1': section1,
-             'section2': section2,
-             'section3': section3,
-             'section4': section4,
-             'section5': section5
-            })
+      section1 = form[facility_name_pos:outpatient_pos]
+      section2 = form[outpatient_pos:lab_pos]
+      section3 = form[lab_pos:physicians_pos]
+      section4 = form[physicians_pos:average_pos]
+      section5 = form[average_pos:]
+
+      return render(request, 'memdb/register.html',
+              {'section1': section1,
+               'section2': section2,
+               'section3': section3,
+               'section4': section4,
+               'section5': section5
+              })
+    elif request.method == 'POST':
+      return HttpResponse(request.POST.get('facility_name') + 'testing')
