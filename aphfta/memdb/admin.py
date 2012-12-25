a2a9e9e3ee9da4ac678077bd0116b13403436365
Brandon Liu <brandon.k.liu@gmail.com>
2012-12-25 13:55:32 +0300

    Customize FacilityAdmin and also move 'date' field to 'date_joined'

--- aphfta/memdb/admin.py
+++ aphfta/memdb/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from models import Facility, OtherStaff, Ward
 
-admin.site.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+  list_display = ('facility_name', 'doctor_ic', 'tel_office', 'email', 'moh_reg_cert')
+  search_fields = ('facility_name', 'tel_office', 'moh_reg_cert')
+  date_hierarchy = 'date_joined'
+  ordering = ('facility_name',)
+
+admin.site.register(Facility, FacilityAdmin)
 admin.site.register(OtherStaff)
 admin.site.register(Ward)
