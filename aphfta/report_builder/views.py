993c5cf856a80b2c1127d62107a6817a9676e23f
Salvatore Rinchiera <srinchiera@college.harvard.edu>
2013-01-14 14:25:26 -0500

    ability to filter out fields from report

--- aphfta/report_builder/views.py
+++ aphfta/report_builder/views.py
@@ -27,8 +27,8 @@ class ReportEditForm(forms.ModelForm):
     class Meta:
         model = Report
         fields = ['name', 'distinct',]
-    
-    
+
+
 class DisplayFieldForm(forms.ModelForm):
     class Meta:
         model = DisplayField
@@ -40,7 +40,7 @@ class DisplayFieldForm(forms.ModelForm):
             'width': forms.TextInput(attrs={'class':'small_input'}),
             'sort': forms.TextInput(attrs={'class':'small_input'}),
         }
-        
+
 
 class FilterFieldForm(forms.ModelForm):
     class Meta:
@@ -57,7 +57,7 @@ class FilterFieldForm(forms.ModelForm):
 class ReportCreateView(CreateView):
     form_class = ReportForm
     template_name = 'report_new.html'
-    
+
 
 def get_relation_fields_from_model(model_class):
     relation_fields = []
@@ -72,13 +72,20 @@ def get_relation_fields_from_model(model_class):
 def get_direct_fields_from_model(model_class):
     direct_fields = []
     all_fields_names = model_class._meta.get_all_field_names()
+    if model_class.__name__.lower() == 'facility':
+        all_fields_names = filter_facility_fields(all_fields_names)
     for field_name in all_fields_names:
         field = model_class._meta.get_field_by_name(field_name)
         # Direct, not m2m, not FK
         if field[2] and not field[3] and field[0].__class__.__name__ != "ForeignKey":
             direct_fields += [field[0]]
     return direct_fields
-            
+
+def filter_facility_fields(fields):
+    fields_to_remove = ['id']
+    filtered_fields = [field for field in fields if not field in fields_to_remove]
+    return filtered_fields
+
 def ajax_get_related(request):
     """ Get related model and fields
     Requires get variables model and field
@@ -89,24 +96,24 @@ def ajax_get_related(request):
     field = model._meta.get_field_by_name(field_name)
     path = request.GET['path']
     path_verbose = request.GET['path_verbose']
-    
+
     if field[2]:
         # Direct field
         new_model = field[0].related.parent_model()
     else:
         # Indirect related field
         new_model = field[0].model()
-    
+
     new_fields = get_relation_fields_from_model(new_model)
     model_ct = ContentType.objects.get_for_model(new_model)
-    
+
     if path_verbose:
         path_verbose += "::"
     path_verbose += field[0].name
-    
+
     path += field_name
     path += '__'
-    
+
     return render_to_response('report_builder/report_form_related_li.html', {
         'model_ct': model_ct,
         'related_fields': new_fields,
@@ -121,29 +128,29 @@ def ajax_get_fields(request):
     model = ContentType.objects.get(pk=request.GET['model']).model_class()
     path = request.GET['path']
     path_verbose = request.GET['path_verbose']
-    
+
     if field_name == '':
         return render_to_response('report_builder/report_form_fields_li.html', {
             'fields': get_direct_fields_from_model(model),
         }, RequestContext(request, {}),)
-    
+
     field = model._meta.get_field_by_name(field_name)
     if path_verbose:
         path_verbose += "::"
     path_verbose += field[0].name
-    
+
     path += field_name
     path += '__'
-    
+
     if field[2]:
         # Direct field
         new_model = field[0].related.parent_model()
     else:
         # Indirect related field
         new_model = field[0].model()
-    
+
     fields = get_direct_fields_from_model(new_model)
-    
+
     return render_to_response('report_builder/report_form_fields_li.html', {
         'fields': fields,
         'path': path,
@@ -169,19 +176,19 @@ def report_to_list(report, user, preview=False):
     Returns list, message in case of issues
     """
     message= ""
-    
+
     model_class = report.root_model.model_class()
-    
+
     objects = model_class.objects.all()
-    
+
     # Filters
     for filter_field in report.filterfield_set.all():
         try:
             filter_string = str(filter_field.path + filter_field.field)
-            
+
             if filter_field.filter_type:
                 filter_string += '__' + filter_field.filter_type
-            
+
             # Check for special types such as isnull
             if filter_field.filter_type == "isnull" and filter_field.filter_value == "0":
                 filter_list = {filter_string: False}
@@ -192,7 +199,7 @@ def report_to_list(report, user, preview=False):
                 else:
                     filter_value = filter_field.filter_value
                 filter_list = {filter_string: filter_value}
-                
+
             if not filter_field.exclude:
                 objects = objects.filter(**filter_list)
             else:
@@ -201,7 +208,7 @@ def report_to_list(report, user, preview=False):
             message += "Filter Error on %s. If you are using the report builder then " % filter_field.field_verbose
             message += "you found a bug! "
             message += "If you made this in admin, then you probably did something wrong."
-    
+
     # Aggregates
     for display_field in report.displayfield_set.filter(aggregate__isnull=False):
         if display_field.aggregate == "Avg":
@@ -212,7 +219,7 @@ def report_to_list(report, user, preview=False):
             objects = objects.annotate(Min(display_field.path + display_field.field))
         elif display_field.aggregate == "Count":
             objects = objects.annotate(Count(display_field.path + display_field.field))
-    
+
     # Ordering
     order_list = []
     for display_field in report.displayfield_set.filter(sort__isnull=False).order_by('sort'):
@@ -221,15 +228,15 @@ def report_to_list(report, user, preview=False):
         else:
             order_list += [display_field.path + display_field.field]
     objects = objects.order_by(*order_list)
-    
+
     # Distinct
     if report.distinct:
         objects = objects.distinct()
-    
+
     # Limit because this is a preview
     if preview:
         objects = objects[:50]
-    
+
     # Display Values
     values_list = []
     for display_field in report.displayfield_set.all():
@@ -260,9 +267,9 @@ def report_to_list(report, user, preview=False):
         message += "Field Error. If you are using the report builder then you found a bug!"
         message += "If you made this in admin, then you probably did something wrong."
         objects_list = None
-    
+
     return objects_list, message
-    
+
 @staff_member_required
 def ajax_preview(request):
     """ This view is intended for a quick preview useful when debugging
@@ -270,12 +277,12 @@ def ajax_preview(request):
     """
     report = get_object_or_404(Report, pk=request.POST['report_id'])
     objects_list, message = report_to_list(report, request.user, preview=True)
-    
+
     return render_to_response('report_builder/html_report.html', {
         'report': report,
         'objects_dict': objects_list,
         'message': message
-        
+
     }, RequestContext(request, {}),)
 
 class ReportUpdateView(UpdateView):
@@ -285,51 +292,51 @@ class ReportUpdateView(UpdateView):
     model = Report
     form_class = ReportEditForm
     success_url = './'
-    
+
     @method_decorator(permission_required('report_builder.change_report'))
     def dispatch(self, request, *args, **kwargs):
         return super(ReportUpdateView, self).dispatch(request, *args, **kwargs)
-    
+
     def get_context_data(self, **kwargs):
         ctx = super(ReportUpdateView, self).get_context_data(**kwargs)
         model_class = self.object.root_model.model_class()
         model_ct = ContentType.objects.get_for_model(model_class)
-        
+
         direct_fields = get_direct_fields_from_model(model_class)
         relation_fields = get_relation_fields_from_model(model_class)
-        
+
         DisplayFieldFormset = inlineformset_factory(
             Report,
             DisplayField,
             extra=0,
             can_delete=True,
             form=DisplayFieldForm)
-        
+
         FilterFieldFormset = inlineformset_factory(
             Report,
             FilterField,
             extra=0,
             can_delete=True,
             form=FilterFieldForm)
-        
+
         if self.request.POST:
             ctx['field_list_formset'] =  DisplayFieldFormset(self.request.POST, instance=self.object)
             ctx['field_filter_formset'] =  FilterFieldFormset(self.request.POST, instance=self.object, prefix="fil")
         else:
             ctx['field_list_formset'] =  DisplayFieldFormset(instance=self.object)
             ctx['field_filter_formset'] =  FilterFieldFormset(instance=self.object, prefix="fil")
-        
+
         ctx['related_fields'] = relation_fields
         ctx['fields'] = direct_fields
         ctx['model_ct'] = model_ct
-        
+
         return ctx
-    
+
     def form_valid(self, form):
         context = self.get_context_data()
         field_list_formset = context['field_list_formset']
         field_filter_formset = context['field_filter_formset']
-        
+
         if field_list_formset.is_valid() and field_filter_formset.is_valid():
             self.object = form.save()
             field_list_formset.report = self.object
@@ -339,7 +346,7 @@ class ReportUpdateView(UpdateView):
             return HttpResponseRedirect(self.get_success_url())
         else:
             return self.render_to_response(self.get_context_data(form=form))
-        
+
 @staff_member_required
 def download_xlsx(request, pk):
     """ Download the full report in xlsx format
@@ -351,12 +358,12 @@ def download_xlsx(request, pk):
     import re
 
     report = get_object_or_404(Report, pk=pk)
-    
+
     wb = Workbook()
     ws = wb.worksheets[0]
     ws.title = report.name
     filename = re.sub(r'\W+', '', report.name) + '.xlsx'
-    
+
     i = 0
     for field in report.displayfield_set.all():
         cell = ws.cell(row=0, column=i)
@@ -364,11 +371,11 @@ def download_xlsx(request, pk):
         cell.style.font.bold = True
         ws.column_dimensions[get_column_letter(i+1)].width = field.width
         i += 1
-    
+
     objects_list, message = report_to_list(report, request.user)
     for row in objects_list:
         ws.append(row)
-    
+
     myfile = StringIO.StringIO()
     myfile.write(save_virtual_workbook(wb))
     response = HttpResponse(
@@ -377,6 +384,3 @@ def download_xlsx(request, pk):
     response['Content-Disposition'] = 'attachment; filename=%s' % filename
     response['Content-Length'] = myfile.tell()
     return response
-    
-    
-    
