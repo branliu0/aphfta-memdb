f3d60d7f3e4c881d7ee704782abf36722ed35721
Brandon Liu <brandon.k.liu@gmail.com>
2013-01-15 12:02:50 +0300

    Make the doctor_ic field editable for now, just to see how it feels

--- aphfta/memdb/admin.py
+++ aphfta/memdb/admin.py
@@ -5,6 +5,7 @@ from helpers.filters import makeSelectFilter, makeBooleanSelectFilter
 
 class FacilityAdmin(admin.ModelAdmin):
   list_display = ('facility_name', 'doctor_ic', 'full_contact', 'region', 'district', 'membership_type', 'membership')
+  list_editable = ('doctor_ic', )
   search_fields = ('facility_name', 'doctor_ic', 'tel_office', 'moh_reg_cert', 'email')
   list_filter = (
       makeSelectFilter('membership_type'),
