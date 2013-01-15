b809994e65e03a28b6d584ce69632d5e75dbfb99
Salvatore Rinchiera <srinchiera@college.harvard.edu>
2013-01-15 08:22:51 -0500

    balance appears in admin with link

--- aphfta/memdb/admin.py
+++ aphfta/memdb/admin.py
@@ -4,7 +4,7 @@ from helpers.filters import makeSelectFilter, makeBooleanSelectFilter
 
 
 class FacilityAdmin(admin.ModelAdmin):
-  list_display = ('facility_name', 'doctor_ic', 'full_contact', 'region', 'district', 'membership_type', 'membership', 'balance')
+  list_display = ('facility_name', 'doctor_ic', 'full_contact', 'region', 'district', 'membership_type', 'membership', 'edit_balance')
   list_editable = ('doctor_ic', )
   search_fields = ('facility_name', 'doctor_ic', 'tel_office', 'moh_reg_cert', 'email')
   list_filter = (
@@ -21,5 +21,13 @@ class FacilityAdmin(admin.ModelAdmin):
       makeBooleanSelectFilter('ambulance'),
   )
   ordering = ('facility_name',)
+
+  def edit_balance(self, obj):
+    if obj.balance == 0:
+        return '<a class="paid balance" href="http://www.yahoo.com">Paid</a>'
+    return '<a class="balance" href="http://www.google.com">%s</a>' % obj.balance
+  edit_balance.allow_tags = True
+
+
 
 admin.site.register(Facility, FacilityAdmin)
