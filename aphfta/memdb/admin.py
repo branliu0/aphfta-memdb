f548854d52f5e13f676b74d0d5968b379290608e
Brandon Liu <brandon.k.liu@gmail.com>
2013-01-16 18:35:38 +0300

    Merge in addresses from Andrew's spreadsheet data. There is still other information from Andrew's spreadsheet that has not been merged in

--- aphfta/memdb/admin.py
+++ aphfta/memdb/admin.py
@@ -5,7 +5,7 @@ from helpers.admin_filters import (makeSelectFilter, makeBooleanSelectFilter,
 
 
 class FacilityAdmin(admin.ModelAdmin):
-  list_display = ('facility_name', 'doctor_ic', 'full_contact', 'region', 'district', 'membership_type', 'membership')
+  list_display = ('facility_name', 'doctor_ic', 'full_contact', 'address', 'region', 'district', 'membership_type', 'membership')
   list_editable = ('doctor_ic', )
   search_fields = ('facility_name', 'doctor_ic', 'tel_office', 'moh_reg_cert', 'email')
   list_filter = (
